5c5307a8177e29cb048d2d33efbda4f8d23a641c
eljhorman06 <jhormandavid06@gmail.com>
2023-03-30 19:58:29 -0500

    Proyecto Formativo

--- Atanasio/view/Noticias/Backend/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-$conn = mysqli_connect('localhost','root','','admin_atanasio');
-session_start();
-
-?>
--- Atanasio/view/html/Logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require_once("../../config/conexion.php");
-    session_destroy();
-    header("Location:".Conectar::ruta()."index.php");
-    exit();
-?>
